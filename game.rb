6ab7f675f954b43b8090a21485c61fdc624f56f2
Jinfeng Lin <linjinfeng@gmail.com>
2012-12-12 10:25:12 +0800

    game01.github.com

---
+++ game.rb
@@ -0,0 +1,14 @@
+class Game
+  
+  def initialize(init_grid)
+    @init_grid = init_grid  
+  end
+  
+  def tick
+    if @init_grid.length == 1
+      [[0]]
+    else
+    [[0], [0]]
+    end
+  end
+end
